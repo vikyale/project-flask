92083c858fe4b9c8bcacb886a352f61bbe89c97c
Victoria Ubaldo <viky.ubaldo@gmail.com>
2020-01-14 14:28:20 -0500

    add template

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask , request,redirect , make_response, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-	return 'Hello World Flask'
+def index():
+	user_ip= request.remote_addr
+	response = make_response(redirect('/hello'))
+	response.set_cookie('user_ip',user_ip)
 	
+	return response
+	
+	
+@app.route('/hello')
+def hello():
+
+	user_ip= request.cookies.get('user_ip')
+	#return 'Hello World Flask!, tu ip es {}'.format(user_ip)
+	return render_template('hello.html',user_ip=user_ip)
+
+
 ##set FLASK_APP=hello.py wn
 ##export FLASK_APP=hello.py lnx 
+##FLASK_DEBUG=1
+##302 code for redirect
