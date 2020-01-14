e7034ed129b9ce95585d662a6c7eea19ff351af8
Victoria Ubaldo <viky.ubaldo@gmail.com>
2020-01-14 17:24:27 -0500

    add context

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask , request,redirect , make_response, render_template
 
 app = Flask(__name__)
 
+todos = ['TODO 1', 'TODO 2','TODO 3']
+
 @app.route('/')
 def index():
 	user_ip= request.remote_addr
@@ -15,8 +17,13 @@ def index():
 def hello():
 
 	user_ip= request.cookies.get('user_ip')
+	context = {
+     'user_ip':user_ip, 
+	 'todos':todos,
+	}
 	#return 'Hello World Flask!, tu ip es {}'.format(user_ip)
-	return render_template('hello.html',user_ip=user_ip)
+	#return render_template('hello.html',user_ip=user_ip, todos=todos)
+	return render_template('hello.html',**context)
 
 
 ##set FLASK_APP=hello.py wn
